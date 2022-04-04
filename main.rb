9f0c758601572e6872653696e403880a624ba3fb
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-04-04 19:44:30 +0000

    Getters, setters and initializing

--- main.rb
+++ main.rb
@@ -1,16 +1,34 @@
 class Student 
-  @first_name
-  @last_name
-  @email
-  @username
-  @password
+  attr_accessor :first_name, :last_name, :email, :username
 
-  def to_s
-  "First Name: #{@first_name}"
+
+  def initialize (firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
   end
-end 
+  
+  
+  # #Setter Method
+  # def first_name=name
+  #   @first_name = name
+  # end 
+
+  # #Getter Method
+  # def first_name
+  #   @first_name
+  # end
+  
+  def to_s
+  "First Name: #{@first_name}, Last Name: #{@last_name}, Username: #{@username}, Email: #{@email}"
+  end
+  
+end
 
 
 
-isaac = Student.new
+isaac = Student.new("Isaac","Rendon","irendon","irendon@gmail.com","password1")
+
 puts isaac
